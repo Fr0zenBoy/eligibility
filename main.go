fdf698f1af14219a0d9f2e9f468ce056924eed29
Fr0zenBoy <th129513@gmail.com>
2023-03-22 22:26:04 -0300

    integration tests

--- main.go
+++ main.go
@@ -1,33 +1,12 @@
 package main
 
-// 1. The transaction amount should not be above limit
-// 2. No transaction should be approved when the card is blocked
-// 3. The first transaction shouldn't be above 90% of the limit
-// 4. There should not be more than 10 transactions on the same merchant
-// 5. Merchant blacklist
-// 6. There should not be more than 3 transactions on a 2 minutes interval
-
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/Fr0zenBoy/authoraizer/controller"
 	"github.com/gin-gonic/gin"
+	"github.com/Fr0zenBoy/authoraizer/server"
 )
 
 func main() {
 	router := gin.New()
-	router.POST("/api/authoraizer", func(ctx *gin.Context) {
-		body := controller.Request{}
-		if err := ctx.ShouldBindJSON(&body); err != nil {
-			ctx.AbortWithStatusJSON(http.StatusBadRequest,
-				gin.H{
-					"error": err.Error(),
-					"message": "Invalid inputs. please check your inputs"})
-			return
-		}
-		fmt.Println(body)
-		ctx.JSON(http.StatusAccepted, body.Allowed(controller.OutPut{}))
-	})
+	router.POST("/api/authoraizer", server.AuthoraizerHandler)
 	router.Run("localhost:8080")
 }
